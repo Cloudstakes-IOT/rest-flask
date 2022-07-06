fa30d3f54722d85ae539f3159994b68f745bed67
ritesh-cloudstakes <107617016+ritesh-cloudstakes@users.noreply.github.com>
2022-07-06 13:26:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/hello', methods=['GET', 'POST'])
-def welcome():
-    return ("Hello World!")
-
-
-if __name__ == '__main__':
-    app.run()
+@app.route("/")
+def index():
+    return "Hello World!"
