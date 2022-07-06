2370967fff89ccb0be85bfab7fb6e0b309f86303
ritesh-cloudstakes <107617016+ritesh-cloudstakes@users.noreply.github.com>
2022-07-06 14:01:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "<h1> Welcome to our Page Cloudstakes<h1>"
+    return "<h1> Welcome to Cloudstakes<h1>"
+
+@post('/esp')
+def index():
+    data = request.body.read()
+    print(data) 
 
 if __name__ == '__main__':
     app.run()
