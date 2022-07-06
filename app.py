dd0d4ce9e6b9fc05a4c9fb4fec424e982be15f98
ritesh-cloudstakes <107617016+ritesh-cloudstakes@users.noreply.github.com>
2022-07-06 16:19:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 def hello():
     return "<h1> Welcome to Cloudstakes<h1>"
 
-@app.route('/post', methods = ["POST"])
+@app.route('/post', methods = ['POST', 'GET']))
 def post():
 
     print(request.data)
