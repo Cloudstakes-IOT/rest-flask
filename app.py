cdb6cab352a792cdc3f7fe81ac51604b90819db7
ritesh-cloudstakes <107617016+ritesh-cloudstakes@users.noreply.github.com>
2022-07-06 14:45:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ def hello():
     return "<h1> Welcome to Cloudstakes<h1>"
 
 @app.route('/post', methods = ["POST"])
-def post():
-
-    print(request.data)
-    return ''   
+def postJsonHandler():
+    content = request.get_json()
+    print (content)
+    return 'JSON posted'
 
 if __name__ == '__main__':
     app.run()
