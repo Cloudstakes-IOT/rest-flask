e0d620c81413ba02a60523fbb156195f3fc229f6
ritesh-cloudstakes <107617016+ritesh-cloudstakes@users.noreply.github.com>
2022-07-06 12:47:48 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/postjson', methods = ['POST'])
+def postJsonHandler():
+    content = request.get_json()
+    print (content)
+    return 'JSON posted'
+
+@app.route('/hello/', methods=['GET', 'POST'])
+def welcome():
+    return "Hello World!"
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', port=105)
